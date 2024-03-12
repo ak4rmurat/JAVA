bcdeee98798c7a51f208ed747564b88abcce3351
ak4rmurat <akar.murat@hotmail.com>
2024-03-12 17:45:24 +0300

    35. gun sonu

--- src/p1/C1.java
+++ src/p1/C1.java
@@ -2,25 +2,8 @@ package p1;
 
 public class C1 {
 
-   private int satis;
-   protected int toplamSatis;
-
-    public void setSatis(int satis) {
-        this.satis = satis;
-        this.toplamSatis += satis;
-    }
-
-   public int getToplamSatis() {
-
-        return toplamSatis;
-    }
-    public void yazi(){
-        System.out.println("ali");
-    }
-
-    public static void main(String[] args) {
-
-    }
-
+   protected void method3(){
+       System.out.println("method3 calsti.");
+   }
 
 }
--- src/p2/C2.java
+++ src/p2/C2.java
@@ -6,11 +6,16 @@ import p1.C1;
 public class C2 extends C1 {
     public static void main(String[] args) {
 
-    C2 obj = new C2();
-    C1 obj1 = new C1();
 
 
 
     }
+    public  void method1(){
 
+
+    }
+
+
+
+    
 }
